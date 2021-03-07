c144aeb48b45ca58595f79e5c9e87d7e8df03184
unknown <ruither.carvalho@gmail.com>
2021-03-06 21:43:35 -0300

    Reusando instancia

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,10 +10,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
+	
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
